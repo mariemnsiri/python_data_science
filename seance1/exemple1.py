ff569185f647bfa874ee87dd39170a65baeea73b
Mariem Nsiri <mariemnsirii900@gmail.com>
2024-02-01 14:39:44 +0100

    first commit

---
+++ seance1/exemple1.py
@@ -0,0 +1,7 @@
+nom="Ali" # variable de type str
+age=30 # variable de type int
+salaire=1200.7654 # variable de type float
+est_celibataire=True # variable de type booléen
+# pour afficher les infos de la personne on appelle print
+print("hello","world", sep=";", end='|')
+print(nom)
