2adeaf53b6b6da6e2ec5736fbffe8f46cb2474e2
Mariem Nsiri <mariemnsirii900@gmail.com>
2024-02-01 16:29:25 +0100

    .gitignore, seance1/notebook1

--- seance1/exemple1.py
+++ seance1/exemple1.py
@@ -4,4 +4,5 @@ salaire=1200.7654 # variable de type float
 est_celibataire=True # variable de type booléen
 # pour afficher les infos de la personne on appelle print
 print("hello","world", sep=";", end='|')
+# le sep supprime l'espace 
 print(nom)
